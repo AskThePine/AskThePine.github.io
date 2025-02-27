58dd8f3950f20d1cd8e1e3ee52459bc62783510c
AskThePine <153175168+AskThePine@users.noreply.github.com>
2025-02-27 20:56:12 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -8,8 +8,8 @@ function setBackgroundImage() {
     var background = document.getElementById('background');
     // 平安夜彩蛋
     if (curDate.getMonth() == 11 && curDate.getDate() == 14) {
-        background.style.backgroundImage = 'url(images/background_Christmas_Eve.jpg)';
+        background.style.backgroundImage = 'url(../images/background_Christmas_Eve.jpg)';
     } else {
-        background.style.backgroundImage = 'url(images/_background.jpg)';
+        background.style.backgroundImage = 'url(../images/_background.jpg)';
     }
 }
